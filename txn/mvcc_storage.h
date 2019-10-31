0b4705c7358943a863269a0ebe7c865ea6c38409
elvinaang <13517079@std.stei.itb.ac.id>
2019-10-31 19:59:23 +0700

    write + check write fix jadi done

--- txn/mvcc_storage.h
+++ txn/mvcc_storage.h
@@ -2,7 +2,7 @@
 #ifndef _MVCC_STORAGE_H_
 #define _MVCC_STORAGE_H_
 
-#include "txn/storage.h"
+#include "storage.h"
 
 // MVCC 'version' structure
 struct Version {
@@ -17,6 +17,8 @@ class MVCCStorage : public Storage {
   // If there exists a record for the specified key, sets '*result' equal to
   // the value associated with the key and returns true, else returns false;
   // The third parameter is the txn_unique_id(txn timestamp), which is used for MVCC.
+  int SearchMaxVersion(Key key, int txn_unique_id = 0);
+
   virtual bool Read(Key key, Value* result, int txn_unique_id = 0);
 
   // Inserts a new version with key and value
@@ -26,28 +28,28 @@ class MVCCStorage : public Storage {
   // Returns the timestamp at which the record with the specified key was last
   // updated (returns 0 if the record has never been updated). This is used for OCC.
   virtual double Timestamp(Key key) {return 0;}
-  
+
   // Init storage
   virtual void InitStorage();
-  
+
   // Lock the version_list of key
   virtual void Lock(Key key);
-  
+
   // Unlock the version_list of key
   virtual void Unlock(Key key);
-  
+
   // Check whether apply or abort the write
   virtual bool CheckWrite (Key key, int txn_unique_id);
-  
+
   virtual ~MVCCStorage();
 
  private:
- 
+
   friend class TxnProcessor;
-  
+
   // Storage for MVCC, each key has a linklist of versions
   unordered_map<Key, deque<Version*>*> mvcc_data_;
-  
+
   // Mutexs for each key
   unordered_map<Key, Mutex*> mutexs_;
 };
